ad87dafe4940fe4bf322ba49b8092d3e091b0619
abebts2000 <54288923+abebts2000@users.noreply.github.com>
2026-04-08 11:07:56 -0400

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>>dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    void mark(vector<vector<char>>& board, int r, int c) {
+        if (r < 0 || c < 0 || r >= board.size() || c >= board[0].size() || board[r][c] != 'O')
+            return;
+        board[r][c] = 'T';
+        for(auto e:dir)
+            mark(board, r + e[0], c +e[1]);
+    }
+
+    void solve(vector<vector<char>>& board) {
+        if (board.empty()) return;
+        int rows = board.size(), cols = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            mark(board, i, 0);
+            mark(board, i, cols - 1);
+        }
+        for (int j = 0; j < cols; j++) {
+            mark(board, 0, j);
+            mark(board, rows - 1, j);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+};
