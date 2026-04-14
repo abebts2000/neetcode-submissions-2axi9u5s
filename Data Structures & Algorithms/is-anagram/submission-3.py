e4402dd29feda68a17e64049c78b1cbf031626f5
abebts2000 <54288923+abebts2000@users.noreply.github.com>
2026-04-14 16:01:46 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # mp = collections.defaultdict(int)
+        # for ch in s:
+        #     mp[ch] += 1
+        # for ch in t:
+        #     mp[ch] -= 1;
+        #     if mp[ch] == 0:
+        #         del mp[ch]
+        # return len(mp) == 0
+        return Counter(s) == Counter(t)
