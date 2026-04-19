3a6cdb687d816bdce7a5180ba0efa238087a0cb4
abebts2000 <54288923+abebts2000@users.noreply.github.com>
2026-04-18 23:08:27 -0400

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        vector<vector<int>>dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),0));
+        for(int i=0;i<obstacleGrid.size();i++)
+        {
+            for(int j =0;j<obstacleGrid[0].size();j++)
+            {
+                if(obstacleGrid[i][j] == 1)
+                {
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    if(i==0 && j==0 && obstacleGrid[i][j] == 0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = (i>0?dp[i-1][j]:0) + (j>0?dp[i][j-1]:0);
+                    
+                }
+            }
+        }
+        return dp[obstacleGrid.size()-1][obstacleGrid[0].size()-1];
+
+    }
+};
